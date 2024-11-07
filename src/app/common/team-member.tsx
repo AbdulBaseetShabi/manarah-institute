5ee47a7df4ef90ddb2d8ac0610f968225a27b521
Abdul-Baseet Shabi <ashabi@atlassian.com>
2024-11-07 09:33:13 -0500

    core team section

---
+++ src/app/common/team-member.tsx
@@ -0,0 +1,27 @@
+interface TeamMemberProp {
+    gender: 'MALE' | 'FEMALE',
+    name: string;
+    role: string;
+}
+
+// TEMP - to be converted to image
+const genderColorMapping: { [key in TeamMemberProp['gender']]: string} = {
+    'MALE': 'bg-blue-200',
+    'FEMALE': 'bg-pink-100'
+}
+
+const TeamMember = ({ gender, role, name }: TeamMemberProp) => {
+  return (
+    <div className="mx-8 my-4">
+      <div className={`w-40 h-40 rounded-full ${genderColorMapping[gender]}`}>
+        
+      </div>
+      <div className="w-full text-center mt-2">
+        <h4 className="font-semibold">{role}</h4>
+        <h5>{name}</h5>
+      </div>
+    </div>
+  );
+};
+
+export default TeamMember;
--- src/app/pages/team/core.tsx
+++ src/app/pages/team/core.tsx
@@ -1,10 +1,15 @@
 import { RouteId } from "../../common/types";
 import SectionLayout from "../../common/layout/section-layout";
+import TeamMember from "../../common/team-member";
 
 const CoreTeam = () => {
   return (
     <SectionLayout id={RouteId.core}>
       <div>This is the Core Team Section</div>
+      <div className="flex flex-wrap justify-around">
+        <TeamMember gender="MALE" name="Furqan Einstein" role="President" />
+        <TeamMember gender="FEMALE" name="Aisha ibn Malik" role="VP of Marketing" />
+      </div>
     </SectionLayout>
   );
 };
