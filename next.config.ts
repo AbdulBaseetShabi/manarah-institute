ba4333d23753ccef5bbaaed202602f2232147952
Abdul-Baseet Shabi <ashabi@atlassian.com>
2026-01-17 21:16:14 -0500

    url fix part 2

--- next.config.ts
+++ next.config.ts
@@ -6,19 +6,11 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'drive.google.com',
-        port: '',
-        pathname: '/uc/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'drive.google.com',
-        port: '',
-        pathname: '/thumbnail',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
-        port: '',
         pathname: '/**',
       },
     ],
